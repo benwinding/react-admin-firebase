acf407c66d794f00ffee06e12e29cf6c9389a033
Ben Winding <ben.winding@gmail.com>
2018-09-15 17:02:13 +0930

    Observables working with firebasegit status!

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,27 @@
 import * as React from 'react';
 import { PostList } from './posts';
 
-import jsonServerProvider from 'ra-data-json-server';
+// import jsonServerProvider from 'ra-data-json-server';
 import { Admin, Resource } from 'react-admin';
+import { FirebaseProvider } from './react-admin-firebase/reactAdminFirebase';
 
-const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+const config = {
+  apiKey: "AIzaSyBJIVBrJ1Ru5pd-wJ1dlCYj6ddq1AAw7xI",
+  authDomain: "test-react-admin-1854c.firebaseapp.com",
+  databaseURL: "https://test-react-admin-1854c.firebaseio.com",
+  projectId: "test-react-admin-1854c",
+  storageBucket: "test-react-admin-1854c.appspot.com",
+  // messagingSenderId: "653484435936",
+};
+
+const firebaseProvider = FirebaseProvider(config, ['posts']);
+// const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 
 class App extends React.Component {
   public render() {
     return (
-      <Admin dataProvider={dataProvider}>
+      // <Admin dataProvider={dataProvider}>
+      <Admin dataProvider={firebaseProvider}>
         <Resource name="posts" list={PostList} />
       </Admin>
     );
---
+++ src/react-admin-firebase/reactAdminFirebase.ts
@@ -0,0 +1,119 @@
+import * as firebase from 'firebase';
+import {
+    CREATE,
+    DELETE,
+    GET_LIST,
+    GET_MANY,
+    GET_MANY_REFERENCE,
+    GET_ONE,
+    UPDATE,
+} from 'react-admin';
+import { Observable } from 'rxjs';
+
+interface IResource {
+  path: string;
+  collection: firebase.firestore.CollectionReference;
+  observable: Observable<{}>;
+  list: Array<{}>;
+}
+
+class FirebaseClient {
+  private db: firebase.firestore.Firestore;
+  private app: firebase.app.App;
+  private resources: IResource[] = [];
+
+  constructor(
+    private firebaseConfig: {},
+    private pathsArray: string[]
+  ){
+    this.app = firebase.initializeApp(this.firebaseConfig);
+    this.db = this.app.firestore();
+    this.pathsArray.forEach((inputPath) => {
+      if (inputPath == null) {
+        return;
+      }
+      const path = inputPath;
+      const collection = this.db.collection(path);
+      const observable = this.getCollectionObservable(collection);
+      observable.subscribe((querySnapshot: firebase.firestore.QuerySnapshot) => {
+        const newList = querySnapshot.docs.map((doc: firebase.firestore.QueryDocumentSnapshot) => {
+          return {
+            ...doc.data(),
+            id: doc.id
+          } 
+        });
+        this.setList(newList, path);
+        // tslint:disable-next-line:no-console
+        console.log(newList);
+      })
+      const list: Array<{}> = [];
+      const r: IResource = {
+        collection,
+        list,
+        observable,
+        path,
+      };
+      this.resources.push(r);
+    })
+  }
+
+  public getList(resourceName: string): Promise<Array<{}>> {
+    return this.tryGetResource(resourceName)
+      .then((resource: IResource) => resource.list)
+   }
+
+  public getTotal(resourceName: string): Promise<number> {
+    return this.tryGetResource(resourceName)
+      .then((resource: IResource) => resource.list.length)
+  }
+
+  private setList(newList: Array<{}>, resourceName: string): Promise<any> {
+    return this.tryGetResource(resourceName)
+      .then((resource: IResource) => {resource.list = newList})
+  }
+
+  private async tryGetResource(resourceName: string) {
+    const matches: IResource[] = this.resources.filter((val) => {
+      return val.path === resourceName;
+    })
+    if (matches.length < 1) {
+      throw new Error('Cant find resource with id');
+    }
+    const match: IResource = matches[0];
+    return match;
+  }
+
+  private getCollectionObservable(collection: firebase.firestore.CollectionReference): Observable<any> {
+    const observable: any = Observable.create((observer: any) => 
+      collection
+      .onSnapshot(observer)
+    );
+    observable.subscribe((querySnapshot: firebase.firestore.QuerySnapshot) => {
+      // tslint:disable-next-line:no-console
+      console.log('getCollectionObservable', querySnapshot)
+    });
+    return observable;
+  }
+}
+
+let fb: FirebaseClient;
+async function providerApi(type: string, resourceName: string, params: {}) {
+  switch(type) {
+    case GET_MANY:
+    case GET_MANY_REFERENCE:
+    case GET_LIST:
+      return { data: await fb.getList(resourceName), total: await fb.getTotal(resourceName) };
+    case GET_ONE:
+    case CREATE:
+    case UPDATE:
+    case DELETE:
+    default:
+      return {};
+  }
+}
+
+export function FirebaseProvider(config: {}, pathList: string[]): any {
+  fb = new FirebaseClient(config, pathList);
+  return providerApi;
+}
+
