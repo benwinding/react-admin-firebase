1bfe36e131f068c1ad5446150e5b6075275edb2d
Ben Winding <ben.winding@gmail.com>
2018-09-16 16:14:30 +0930

    Adding types

---
+++ src/react-admin-firebase/messageTypes.ts
@@ -0,0 +1,98 @@
+interface IParamsGetList {
+  pagination: {
+    page: number,
+    perPage: number    
+  },
+  sort: {
+    field: string,
+    order: string
+  }
+  filter: {}
+}
+
+interface IParamsGetOne {
+  id: string
+}
+
+interface IParamsCreate {
+  data: {}
+}
+
+interface IParamsUpdate {
+  id: string,
+  data: {},
+  previousData: {},
+}
+
+interface IParamsUpdateMany {
+  ids: string[],
+  data: {}
+}
+
+interface IParamsDelete {
+  id: string,
+  previousData: {},
+}
+
+interface IParamsDeleteMany {
+  ids: string[],
+}
+
+interface IParamsGetMany {
+  ids: string[],
+}    
+
+interface IParamsGetManyReference {
+  pagination: {
+    page: number,
+    perPage: number    
+  },
+  sort: {
+    field: string,
+    order: string
+  }
+  filter: {}
+}    
+
+
+
+
+interface IResponseGetList {
+  data: Array<{}>,
+  total: number
+}
+
+interface IResponseGetOne {
+  data: {}
+}
+
+interface IResponseCreate {
+  data: {}
+}
+
+interface IResponseUpdate {
+  data: {}  
+}
+
+interface IResponseUpdateMany {
+  data: Array<{}>
+}
+
+interface IResponseDelete {
+  data: {}
+}
+
+interface IResponseDeleteMany {
+  data: Array<{}>
+}
+
+interface IResponseGetMany {
+  data: Array<{}>
+}    
+
+interface IResponseGetManyReference {
+  data: Array<{}>,
+  total: number
+}    
+
+
