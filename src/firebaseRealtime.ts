08a39bb4d6fad4b7e8b5c6fad7fa131d8ac91a2f
Ben Winding <ben.winding@gmail.com>
2018-09-18 14:26:07 +0930

    Realtime saga working nicely

--- src/firebaseRealtime.ts
+++ src/firebaseRealtime.ts
@@ -1,34 +1,39 @@
-// In createRealtimeSaga.js
-import { fb } from './';
+import realtimeSaga from "ra-realtime";
+import {
+  fb
+} from './index';
 
-const observeRequest = (firebaseProvider: any) => (
-  type: any,
-  resource: any,
-  params: any
-) => {
-  console.log("REALTIME!!", { type, resource, params });
-  // Filtering so that only posts are updated in real time
+const observeRequest = (dataProvider, options) => (type, resource, params) => {  
+  // If the paths are explicitly set in options
+  if (options && Array.isArray(options.observe) && !options.observe.includes(resource)) {
+    // Then don't observe it, if it's not set
+    return;
+  }
 
   // Use your apollo client methods here or sockets or whatever else including the following very naive polling mechanism
   return {
-    subscribe(observer: any) {
-      const r = fb.GetResource(resource);
-      // const subscription = r.realtimeObservable.subscribe((newList) => {
-      //   observer.next(newList);
-      // });
-      const sub = {
-        // unsubscribe() {
-        //   subscription.unsubscribe();
-        //   // Notify the saga that we cleaned up everything
-        //   observer.complete();
-        // }
+    subscribe(observer) {
+      const resourceObj = fb.GetResource(resource);
+      const sub = resourceObj.observable.subscribe(() => {
+        dataProvider(type, resource, params)
+          .then(results => observer.next(results)) // New data received, notify the observer
+          .catch(error => observer.error(error)); // Ouch, an error occured, notify the observer
+      });
+
+      const subscription = {
+        unsubscribe() {
+          sub.unsubscribe();
+          // Notify the saga that we cleaned up everything
+          // observer.complete();
+          // ^ THIS FAILS FRAMEWORK ISSUE
+        }
       };
-      return sub;
-    }    
+
+      return subscription;
+    }
   };
 };
 
-export default (dataProvider: any) => {
-  console.log({dataProvider});
-  return observeRequest(dataProvider);
-};
+export default (dataProvider, options) => {
+  return realtimeSaga(observeRequest(dataProvider, options));
+}
