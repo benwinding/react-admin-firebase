e2ad2c080862d4b96815aee021ad7aae5a787282
Ben Winding <ben.winding@gmail.com>
2019-05-27 00:35:48 +0930

    Remove realtime provider from demo

--- src-demo/src/App.js
+++ src-demo/src/App.js
@@ -2,7 +2,6 @@ import * as React from "react";
 import { PostList, PostShow, PostCreate, PostEdit } from "./posts";
 import { Admin, Resource } from "react-admin";
 import {
-  FirebaseRealTimeSaga,
   FirebaseDataProvider,
   FirebaseAuthProvider
 } from "react-admin-firebase";
@@ -12,16 +11,11 @@ config.debug = true;
 
 const authProvider = FirebaseAuthProvider(config);
 const dataProvider = FirebaseDataProvider(config);
-const options = {
-  observe: ["posts"]
-};
-const firebaseRealtime = FirebaseRealTimeSaga(dataProvider, options);
 
 class App extends React.Component {
   render() {
     return (
       <Admin
-        customSagas={[firebaseRealtime]}
         dataProvider={dataProvider}
         authProvider={authProvider}
       >
