1cafa10485a609a157aa608273a54f6aa5020035
Ben Winding <ben.winding@gmail.com>
2019-06-27 00:16:46 +0930

    Better dev system

--- src-demo/webpack-override.js
+++ src-demo/webpack-override.js
@@ -265,7 +265,9 @@ module.exports = {
     // to restart the development server for Webpack to discover it. This plugin
     // makes the discovery automatic so you don't have to restart.
     // See https://github.com/facebookincubator/create-react-app/issues/186
+
     new WatchMissingNodeModulesPlugin(paths.appNodeModules),
+
     // Moment.js is an extremely popular library that bundles large locale files
     // by default due to how Webpack interprets its code. This is a practical
     // solution that requires the user to opt into importing specific locales.
