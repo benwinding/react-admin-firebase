ee4606c0d7fdaa47d20562c18fb37a695d5e6f98
Ben Winding <ben.winding@gmail.com>
2019-06-27 00:16:56 +0930

    Remove cruft in types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,18 +1,10 @@
-// declare module 'indefinite-observable'  {
-//   var x: any;
-//   export = x;
-// }
 declare module 'csstype';
-// declare module 'rxjs';
-
 declare module '@material-ui/core/AppBar';
 declare module 'react-admin-firebase';
 
 declare module 'firebase/app';
 declare module 'react-admin';
 declare module 'ra-realtime';
-// declare module 'ra-input-rich-text';
-// declare module 'react-admin-firebase';
 
 declare module 'path-browserify' {
   import path from 'path'
