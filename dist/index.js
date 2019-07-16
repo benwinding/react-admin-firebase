7174a79f7af521e28f2f91f2545876f46bed94dd
hffmnn <hffmnn@users.noreply.github.com>
2019-07-16 14:43:45 +0200

    export new key

--- dist/index.js
+++ dist/index.js
@@ -4,3 +4,4 @@ var firebaseRealtimeSaga_1 = require("./firebaseRealtimeSaga");
 exports.FirebaseRealTimeSaga = firebaseRealtimeSaga_1.default;
 var firebaseDataProvider_1 = require("./firebaseDataProvider");
 exports.FirebaseDataProvider = firebaseDataProvider_1.default;
+exports.CREATE_WITHOUT_AUTOMATIC_ID_KEY = firebaseDataProvider_1.CREATE_WITHOUT_AUTOMATIC_ID_KEY;
