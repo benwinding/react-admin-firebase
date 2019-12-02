623bc6ac4cb87812e83d9edc02ed8381d0d111b7
Simon Buerger <simon@bluegrassdigital.com>
2019-12-02 14:26:54 +0200

    react-admin 3.0

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-import { RealtimeSaga } from "./providers/RealtimeSaga";
 import { DataProvider } from "./providers/DataProvider";
 import { AuthProvider } from "./providers/AuthProvider";
 import { RAFirebaseOptions } from "./providers/RAFirebaseOptions";
 
 export {
-  RealtimeSaga as FirebaseRealTimeSaga,
   DataProvider as FirebaseDataProvider,
   AuthProvider as FirebaseAuthProvider,
   RAFirebaseOptions as RAFirebaseOptions
