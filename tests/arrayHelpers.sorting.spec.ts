b9b19de90ca42991a06022bf2bc6708208e12102
Ben Winding <hello@benwinding.com>
2020-04-25 16:42:36 +0930

    Testing passes

---
+++ tests/arrayHelpers.sorting.spec.ts
@@ -0,0 +1,17 @@
+import { sortArray } from "../src/misc";
+
+describe("array sort", () => {
+  test("returns an ascending array", () => {
+    const input = [{ a: 1 }, { a: 2 }];
+    const expected = [{ a: 1 }, { a: 2 }];
+    sortArray(input, "a", "asc");
+    expect(input).toEqual(expected);
+  });
+
+  test("returns an descending array", () => {
+    const input = [{ a: 1 }, { a: 2 }];
+    const expected = [{ a: 2 }, { a: 1 }];
+    sortArray(input, "a", "desc");
+    expect(input).toEqual(expected);
+  });
+});
--- tests/integration-tests/ApiGetList.spec.ts
+++ tests/integration-tests/ApiGetList.spec.ts
@@ -25,4 +25,36 @@ describe("api methods", () => {
     });
     expect(result.data.length).toBe(3);
   }, 100000);
+
+  test("FirebaseClient list docs", async () => {
+    const testDocs = [
+      {
+        title: "A",
+        isEnabled: false,
+      },
+      {
+        title: "B",
+        isEnabled: true,
+      },
+      {
+        title: "C",
+        isEnabled: false,
+      },
+    ];
+    const collName = "list-filtered";
+    const collection = fire.db().collection(collName);
+    await Promise.all(testDocs.map((doc) => collection.add(doc)));
+
+    const client = new FirebaseClient(fire, {});
+    const result = await client.apiGetList(collName, {
+      pagination: {
+        page: 1,
+        perPage: 10,
+      },
+      filter: {
+        isEnabled: false
+      } as any
+    });
+    expect(result.data.length).toBe(2);
+  }, 100000);
 });
