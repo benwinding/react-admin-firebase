79c03d940ef0060601fc9497fc01d9fd49414e75
Ben Winding <hello@benwinding.com>
2020-04-30 22:38:45 +0930

    Cleaned up src-demo patches

---
+++ src-demo/overrides/patch.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const fs = require("fs");
+
+const destDir = path.join(__dirname, "../node_modules/react-scripts/config");
+
+// fs.copyFileSync(
+//   path.join(__dirname, "./webpack.config.prod.js"),
+//   path.join(destDir, "./webpack.config.prod.js")
+// );
+fs.copyFileSync(
+  path.join(__dirname, "./webpack.config.dev.js"),
+  path.join(destDir, "./webpack.config.dev.js")
+);
+fs.copyFileSync(
+  path.join(__dirname, "./webpackDevServer.config.js"),
+  path.join(destDir, "./webpackDevServer.config.js")
+);
