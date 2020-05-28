9049c970e06df9d94b337e6d83aa0a662c68d54b
Ben Winding <hello@benwinding.com>
2020-05-28 23:48:41 +0930

    Fix source-maps not working properly in src-demo

--- src-demo/overrides/patch.js
+++ src-demo/overrides/patch.js
@@ -3,6 +3,10 @@ const fs = require("fs");
 
 const destDir = path.join(__dirname, "../node_modules/react-scripts/config");
 
+console.log(
+  'patching webpack so "react-firebase-admin" is watched for changes'
+);
+
 // fs.copyFileSync(
 //   path.join(__dirname, "./webpack.config.prod.js"),
 //   path.join(destDir, "./webpack.config.prod.js")
