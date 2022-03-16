2024ff8afe886d2ee6ba72a57ba8651f44347420
Ben Winding <hello@benwinding.com>
2022-03-16 17:30:45 +1100

    Implemented the reference document parsing Fixes #123

---
+++ src/misc/internal.models.ts
@@ -0,0 +1,6 @@
+export const REF_INDENTIFIER = "___REF_FULLPATH_";
+
+export interface ParsedRefDoc {
+  ___refpath: string;
+  ___refid: string;
+}
--- src/providers/queries/GetOne.ts
+++ src/providers/queries/GetOne.ts
@@ -1,5 +1,5 @@
 import { FireClient } from '../database/FireClient';
-import { log, recursivelyMapStorageUrls } from '../../misc';
+import { log, translateDocFromFirestore } from '../../misc';
 import * as ra from '../../misc/react-admin-models';
 
 export async function GetOne<T extends ra.Record>(
@@ -7,14 +7,13 @@ export async function GetOne<T extends ra.Record>(
   params: ra.GetOneParams,
   client: FireClient
 ): Promise<ra.GetOneResult<T>> {
-  log('GetOne', { resourceName, params });
-  const { rm, fireWrapper } = client;
+  log("GetOne", { resourceName, params });
+  const { rm } = client;
   try {
     const id = params.id + '';
     const dataSingle = await rm.GetSingleDoc(resourceName, id);
     client.flogger.logDocument(1)();
-    const data = await recursivelyMapStorageUrls(fireWrapper, dataSingle);
-    return { data: data };
+    return { data: dataSingle as T };
   } catch (error) {
     throw new Error(
       'Error getting id: ' + params.id + ' from collection: ' + resourceName
