c707ef67f06483ca519cfa8f87d8778e757e611c
Matheus Moura <matheus.moura13@gmail.com>
2022-12-06 15:08:21 -0300

    Add storage parser test

---
+++ tests/storagePath.spec.ts
@@ -0,0 +1,20 @@
+import { parseStoragePath } from '../src/misc';
+
+test('useFileNamesInStorage: false', () => {
+  const mockFile = new File([], "test.png");
+  const docPath = "resource/id"
+  const fieldPath = "picture"
+
+  const path = parseStoragePath(mockFile, docPath, fieldPath, false);
+  expect(path).toBe('resource/id/picture.png')
+});
+
+test('useFileNamesInStorage: true', () => {
+  const mockFile = new File([], "test.png");
+  const docPath = "resource/id"
+  const fieldPath = "picture"
+
+  const path = parseStoragePath(mockFile, docPath, fieldPath, true);
+  expect(path).toBe('resource/id/picture/test.png')
+});
+
