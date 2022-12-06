a24be93ebdcbc9f10c7ed815a063cedd9a7e472d
Matheus Moura <matheus.moura13@gmail.com>
2022-12-06 15:08:10 -0300

    Add storage path parser

---
+++ src/misc/storage-parser.ts
@@ -0,0 +1,20 @@
+import { joinPaths } from "./pathHelper";
+
+export function parseStoragePath(
+  rawFile: File,
+  docPath: string,
+  fieldPath: string,
+  useFileName: boolean
+): string {
+  const fileNameBits = rawFile instanceof File
+    ? rawFile.name.split('.')
+    : [];
+
+  const fileExtension = !fileNameBits?.length
+    ? ''
+    : '.' + fileNameBits.pop();
+
+  return useFileName
+    ? joinPaths(docPath, fieldPath, rawFile.name)
+    : joinPaths(docPath, fieldPath + fileExtension);
+}
